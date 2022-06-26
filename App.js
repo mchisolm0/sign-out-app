f623135ee13a3626ab8263917be7e649e6ad7670
mchisolm0 <mchisolm0@gmail.com>
2022-06-26 03:02:20 +0000

    Imported needed commponents from libraries etc.
    I imported the necessary React Native Navigation Libraries and the custom screens I made in ./Modules/Screens.js (though there seems to be a problem with that one).

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen, DetailScreen } from './Modules/Screens';
 
 export default function App() {
   return (
