5fd7fd7da9ce0fd7f9ac6720351c13a4d6c8358c
mchisolm0 <mchisolm0@gmail.com>
2022-06-26 21:22:25 +0000

    Updated imports for App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen, DetailScreen } from './Modules/Screens';
