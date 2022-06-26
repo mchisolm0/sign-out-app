7af94968063607e36d42e78e760b40585d7eef1d
mchisolm0 <mchisolm0@gmail.com>
2022-06-26 21:16:06 +0000

    Updated Screen components
    I updated HomeScreen and DetailScreen to have `TextInput` and `Button` between the screens.

--- Modules/Screens.js
+++ Modules/Screens.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
+    const [name, setName] = React.useState('');
+
     return (
         <View>
-            <Text>Home Screen</Text>
+            <Text style={styles.headingText}>Home Screen</Text>
+            <TextInput
+                style={styles.userText}
+                placeholder='Please add text'
+                value={name}
+                onChangeText={(text)=> setName(text)} />
+            <Button title='Go to detail' style={styles.button} onPress={() => navigation.navigate("Details", { name })} />
         </View>
     )
 }
 
-function DetailScreen() {
+function DetailScreen({ route, navigation }) {
+    const { name } = route.params;
+
     return (
         <View>
-            <Text>Detail Screen</Text>
+            <Text style={styles.headingText}>Student: { name } </Text>
+            <Button title='Go Home' style={styles.button} onPress={() => navigation.navigate('Home')} />
         </View>
     )
 }
