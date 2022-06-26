d69cf2290f33bac81ba2a35ca95db0c3415d16e1
mchisolm0 <mchisolm0@gmail.com>
2022-06-26 02:43:22 +0000

    Added new file for designing Screens
    Currently, added a home screen placeholder (HomeScreen) and a detail screen placeholder (DetailScreen).

---
+++ Modules/Screens.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+function HomeScreen() {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
+
+function DetailScreen() {
+    return (
+        <View>
+            <Text>Detail Screen</Text>
+        </View>
+    )
+}
