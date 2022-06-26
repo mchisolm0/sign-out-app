3111a7471d1125b7fd4eb58eecc0257a67df4359
mchisolm0 <mchisolm0@gmail.com>
2022-06-26 21:18:38 +0000

    Added style for buttons and TextInput
    It seems like the buttons are failing to take their styles for some reason.

--- Modules/Screens.js
+++ Modules/Screens.js
@@ -28,4 +28,31 @@ function DetailScreen({ route, navigation }) {
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    button: {
+        padding: 20,
+        backgroundColor: 'blue',
+    },
+    headingText: {
+        fontSize: 36,
+    },
+    userText: {
+        textAlign: 'center',
+        fontSize: 28,
+        padding: 15,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginHorizontal: 16,
+    },
+    textInputContainer: {
+        alignItems:'center'
+    }
+  });
+
 export { HomeScreen, DetailScreen };
