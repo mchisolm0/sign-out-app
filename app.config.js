e6228c94e33f0f52968ad91d5bb8fde7b12d013f
Matthew Chisolm <mchisolm0@gmail.com>
2022-09-10 08:19:38 -0500

    remove firebase config values

--- app.config.js
+++ app.config.js
@@ -1,4 +1,6 @@
-{
+import 'dotenv/config';
+
+export default {
   "expo": {
     "name": "sign-out-app",
     "slug": "sign-out-app",
@@ -29,5 +31,13 @@
     "web": {
       "favicon": "./assets/favicon.png"
     }
+  },
+  "extra": {
+    firebaseApiKey: process.env.FIREBASE_API_KEY,
+    firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+    firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    firebaseAppId: process.env.FIREBASE_APP_ID
   }
 }
--- config/firebase.ts
+++ config/firebase.ts
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import Constants from 'expo-constants';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBIjqgIDsospDvGV9gOHu88jhYodXD6t3M",
-  authDomain: "sign-out-app-43e63.firebaseapp.com",
-  projectId: "sign-out-app-43e63",
-  storageBucket: "sign-out-app-43e63.appspot.com",
-  messagingSenderId: "918271535683",
-  appId: "1:918271535683:web:3ba4e12f7462a1078002f6"
+  apiKey: Constants.manifest?.extra?.firebaseApiKey,
+  authDomain: Constants.manifest?.extra?.firebaseProjectId,
+  projectId: Constants.manifest?.extra?.firebaseProjectId,
+  storageBucket: Constants.manifest?.extra?.firebaseStorageBucket,
+  messagingSenderId: Constants.manifest?.extra?.firebaseMessagingSenderId,
+  appId: Constants.manifest?.extra?.firebaseAppId
 };
 
 const app = initializeApp(firebaseConfig);
