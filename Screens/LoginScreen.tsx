6e11557536063358c545739b7350b320fe390a5c
Matthew Chisolm <mchisolm0@gmail.com>
2022-09-17 07:36:33 -0500

    made small change to login screen

--- Screens/LoginScreen.tsx
+++ Screens/LoginScreen.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 function LoginScreen() {
     return (
-      <View>
-
+      <View style={styles.container}>
+        <Text>
+          This is the login screen.
+        </Text>
       </View>
     );
 }
+
+const styles = StyleSheet.create({ 
+  container: {
+    flex: 1,
+  }
+ });
 
 export default LoginScreen;
